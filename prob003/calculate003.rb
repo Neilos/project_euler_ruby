33dcf01fc77c43b360eca999a159b8481fff23ba
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-05-21 00:00:41 +0100

    solve problem003 - largest prime factor

--- prob003/calculate003.rb
+++ prob003/calculate003.rb
@@ -2,7 +2,7 @@ require 'prime'
 
 class Calculate
   def self.largest_prime_factor(n)
-    n.downto(1) do |i|
+    Math.sqrt(n).to_i.downto(1) do |i|
       if n % i ==0
         if Prime.prime?(i)
           return i 
