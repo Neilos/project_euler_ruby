d4be3eab1d6b029f27299ac4c3f48ff44998be50
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-05-22 17:34:15 +0100

    corrected problem 003  algorithm

--- prob003/calculate003.rb
+++ prob003/calculate003.rb
@@ -1,14 +1,24 @@
 require 'prime'
 
 class Calculate
+  
   def self.largest_prime_factor(n)
-    Math.sqrt(n).to_i.downto(1) do |i|
+    
+    (n/2).to_i.downto(1) do |i|
       if n % i ==0
         if Prime.prime?(i)
           return i 
         end
       end
     end
+
   end
+  
+  def self.is_prime?(x)
+    Math.sqrt(x).to_i.downto(2).all? do |divisor|
+      x % divisor != 0 
+    end
+  end
+
 end
 
