893b4e93537438277a902629ce4bf652da0bad31
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-05-24 17:19:08 +0100

    faster algorithm

--- prob003/calculate003.rb
+++ prob003/calculate003.rb
@@ -1,24 +1,27 @@
-require 'prime'
-
 class Calculate
-  
+
   def self.largest_prime_factor(n)
-    
-    (n/2).to_i.downto(1) do |i|
-      if n % i ==0
-        if Prime.prime?(i)
-          return i 
-        end
+    return 1 if n ==1
+    prime_factors = []
+    divisor = 2
+    cap = n
+    while cap > 1
+      if n % divisor == 0
+        prime_factors << divisor  # smallest factor will necessarily be prime, 
+                                  # rest of algorithm (i.e. use of cap and incrementing divisor) ensures that 
+                                  # next factor is also prime and larger than the previous
+
+        cap = cap / divisor     # 'cap / divisor' could also be prime 
+                                # but there cannot be primes higher than this
+                                # because there is only one 'prime signature'
+                                # for any one number and if 'cap / divisor'
+                                # is prime then there would only two prime factors.
       end
+      divisor = divisor + 1     # increment divisor  
     end
 
-  end
-  
-  def self.is_prime?(x)
-    Math.sqrt(x).to_i.downto(2).all? do |divisor|
-      x % divisor != 0 
-    end
+    puts "prime_factors: #{prime_factors}"
+    prime_factors[-1] if prime_factors.length > 0
   end
 
 end
-
--- prob003/test_calculate003.rb
+++ prob003/test_calculate003.rb
@@ -9,15 +9,15 @@ class TestCalc < Test::Unit::TestCase
     assert_equal 13, Calculate.largest_prime_factor(26)
   end
 
-  def test_is_prime
-    primes = [2, 3, 5, 7, 11, 13, 17, 19]
-    non_primes = [1, 4, 6, 8, 9, 10, 14, 15, 16, 18, 20]
+  # def test_is_prime
+  #   primes = [2, 3, 5, 7, 11, 13, 17, 19]
+  #   non_primes = [1, 4, 6, 8, 9, 10, 14, 15, 16, 18, 20]
     
-    assert primes.all? do |n|
-      Calculate.is_prime?(n)
-    end
-    assert non_primes.all? do |n|
-      !Calculate.is_prime?(n)
-    end
-  end
+  #   assert primes.all? do |n|
+  #     Calculate.is_prime?(n)
+  #   end
+  #   assert non_primes.all? do |n|
+  #     !Calculate.is_prime?(n)
+  #   end
+  # end
 end
