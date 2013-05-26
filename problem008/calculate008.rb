9d7fc125724dca4976c4a37d883572377d771add
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-05-26 22:37:36 +0100

    refactored solution to problem 008 (used each_cons)

--- problem008/calculate008.rb
+++ problem008/calculate008.rb
@@ -1,11 +1,8 @@
 class Calculate
   def self.largest_product_in_series(series_length, n_digit_string)
-    n_digit_string = n_digit_string.to_s
-    number_of_series = n_digit_string.length - series_length
-    products_array = (0..number_of_series).map do |i|
-      n_digit_string[i, series_length].split('').inject do |product, digit|
-        product.to_i * digit.to_i
-      end
+    products_array = []
+    n_digit_string.to_s.split('').each_cons(series_length) do |series|
+      products_array << series.inject { |product, digit| product.to_i * digit.to_i }
     end
     products_array.max
   end
