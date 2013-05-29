cb500252e5432442e42bc74acf239d39b58f92af
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-05-29 21:15:51 +0100

    corrected algorithm for solution to problem 13

--- problem013/calculate013.rb
+++ problem013/calculate013.rb
@@ -1,7 +1,8 @@
 class Calculate
   def self.sum(numbers)
-    numbers.inject do |sum, num|
+    sum = numbers.inject do |sum, num|
       sum += num
     end
+    sum = sum.to_s[0..9].to_i
   end
 end
