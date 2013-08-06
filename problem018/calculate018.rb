a69ff8b6db498c31390f4cba19987db5c6cc99d4
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-08-06 19:42:59 +0100

    problem 18 solved

--- problem018/calculate018.rb
+++ problem018/calculate018.rb
@@ -1,6 +1,34 @@
-class Calculate
+class Triangle
+  def initialize(triangle_array)
+    @triangle_top = Position.new(0, 0, triangle_array)
+  end
 
-def self.some_method
+  def calculate_max_path_sum
+    @triangle_top.max_path_sum
+  end
 end
 
+
+class NilClass
+  def max_path_sum
+    0
+  end
+end
+
+
+class Position
+  attr_accessor :max_path_sum
+  attr_reader :value, :route1, :route2
+  
+  def initialize(row, column, triangle)
+    @value = triangle[row][column]
+    if triangle[row + 1]
+      @route1 = Position.new(row + 1, column, triangle) if triangle[row + 1][column]
+      @route2 = Position.new(row + 1, column + 1, triangle) if triangle[row + 1][column + 1]
+    end
+  end
+
+  def max_path_sum
+    value + [route1.max_path_sum, route2.max_path_sum].max
+  end
 end
--- problem018/test_calculate018.rb
+++ problem018/test_calculate018.rb
@@ -1,11 +1,19 @@
-require 'minitest'
-require 'minitest/autorun'
-require 'minitest/spec'
+require 'rspec'
 require './calculate018'
 
-describe Calculate do
-
-  it "should..." do
+describe Triangle do
+  describe "max_path_sum" do
+    it "should return the sum of values on the maximum path" do
+      triangle_array = [
+        [3],
+        [7, 4],
+        [2, 4, 6],
+        [8, 5, 9, 3]
+      ]
+      tri = Triangle.new(triangle_array)
+      puts tri.calculate_max_path_sum
+      tri.calculate_max_path_sum.should == 23
+    end
   end
-  
 end
+
