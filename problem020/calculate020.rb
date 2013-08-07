bf14ce036e672a6b922497f9b872bc3aff485773
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-08-07 13:16:14 +0100

    problem 20 solved

--- problem020/calculate020.rb
+++ problem020/calculate020.rb
@@ -1,6 +1,14 @@
 class Calculate
 
-def self.some_method
-end
+  def self.sum_of_digits(number)
+    number.to_s.split('').inject(0) do |sum, digit|
+      sum + digit.to_i
+    end
+  end
+
+  def self.factorial(n)
+    return 1 if n ==0
+    (1..n).inject(:*)
+  end
 
 end
--- problem020/exec_calculate020.rb
+++ problem020/exec_calculate020.rb
@@ -1,7 +1,8 @@
 require_relative 'calculate020'
 
-argument = ARGV[0].to_i || 
+argument = ARGV[0].to_i || 100
 
 puts <<TEXT
+The sum of the digits of #{argument}! is: #{Calculate.sum_of_digits(Calculate.factorial(argument))}
 
 TEXT
